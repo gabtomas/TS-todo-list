ddb46749043433dab56370500ab731dd5cc8fefc
gabtomas <46448735+gabtomas@users.noreply.github.com>
2023-07-02 12:17:42 +0200

    feat: addListItem

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,47 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import confetti from 'canvas-confetti';
+import { v4 as uuidV4 } from 'uuid';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const id = uuidV4();
+console.log(`Hello, ${id}!`);
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', (event) => {
+  // prevent the default behavior of the form submit event
+  event.preventDefault();
+
+  //if input exists give me the value, if not return undefined
+  if (input?.value == '' || input?.value == null) {
+    return;
+  }
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  addListItem(task);
+});
+
+function addListItem(task: Task) {
+  const listItem = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.append(checkbox, task.title);
+  listItem.append(label);
+  list?.append(listItem);
+}
+
+interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}
